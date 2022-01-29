a4e30c45d0215c4f0c66c8870dc4c45bba063f10
Chaitumahato <98262224+Chaitumahato@users.noreply.github.com>
2022-01-29 08:53:25 +0530

    Add files via upload

---
+++ case.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+main()
+{
+	int i=3;
+	switch(i)
+	{
+		case 1:
+			printf("hello");
+			break;
+		case 3+0:
+			printf("hi");
+			break;
+		defalut:
+			printf("chaitu");
+	}
+}
